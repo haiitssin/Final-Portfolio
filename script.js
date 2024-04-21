4a1828c243a6146012c5f6a49b9d917a67a368d3
Anna <annaliu1284@gmail.com>
2024-04-20 20:24:02 -0600

    Commit 1
    I got most of the main elements and layout stuff to work and format correctly.
    
    - 2 working pages but ill be adding more.
    - need to add the event handler thingy
    - might move the accordion to a designated about me page if everything goes smoothly
    - still need to adjust the styles and change the youtube video
    - omg its so organized and its going so smoothly hehehehehehe

---
+++ script.js
@@ -0,0 +1,89 @@
+
+//accordio
+document.addEventListener('DOMContentLoaded', function () {
+    const accordions = document.querySelectorAll('.accordion');
+  
+    accordions.forEach((accordion) => {
+      accordion.addEventListener('click', function () {
+        this.classList.toggle('active');
+        const panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+      });
+    });
+  });
+
+
+  
+// scroll reveal and what it applies to
+  window.addEventListener('DOMContentLoaded', function() {
+    // apply to hero title
+    ScrollReveal().reveal('#heroTitle', {
+        delay: 300,  
+        distance: '50px', 
+        origin: 'top', 
+        easing: 'ease-in-out', 
+        duration: 1000, 
+        reset: true 
+    });
+    // apply to hero description
+    ScrollReveal().reveal('#heroDescription', {
+        delay: 500,
+        distance: '50px',
+        origin: 'bottom',
+        easing: 'ease-in-out',
+        duration: 1000,
+        reset: true
+    });
+
+    // appy to .card
+    ScrollReveal().reveal('.card', {
+        delay: 300,
+        distance: '50px',
+        origin: 'bottom',
+        easing: 'ease-in-out',
+        duration: 1000,
+        reset: true
+    });
+   // apply to main section
+   ScrollReveal().reveal('.content > *', {
+    delay: 300,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    duration: 1000,
+    reset: true
+});
+    //apply to .card-container 
+ScrollReveal().reveal('.card-container > *', {
+    delay: 300,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    duration: 1000,
+    reset: true
+});
+
+    // apply to accordion
+    ScrollReveal().reveal('.accordion', {
+        delay: 300,
+        distance: '50px',
+        origin: 'bottom',
+        easing: 'ease-in-out',
+        duration: 1000,
+        reset: true,
+        once: true // Trigger the animation only once but its not working?
+    });
+    
+});
+
+
+
+// changes the year to current
+var currentYear = new Date().getFullYear();
+// Sets the year
+document.getElementById("currentYear").textContent = currentYear;
+
